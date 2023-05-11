9b9fb767ba541c6f78583df46d5efc0b32a5aad7
Amutha Muhunthan <tamamutha@gmail.com>
2023-05-11 12:41:47 +1000

    loggin in and adding person on system is working this is not saving an data directly on sever

--- graphql-mongoDb-phonebook-backend/index.js
+++ graphql-mongoDb-phonebook-backend/index.js
@@ -13,7 +13,8 @@ const MONGODB_URI = process.env.MONGODB_URI
 
 console.log('connecting to', MONGODB_URI)
 
-mongoose.connect(MONGODB_URI)
+mongoose
+  .connect(MONGODB_URI)
   .then(() => {
     console.log('connected to MongoDB')
   })
@@ -21,27 +22,26 @@ mongoose.connect(MONGODB_URI)
     console.log('error connection to MongoDB:', error.message)
   })
 
-
 let persons = [
   {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+    name: 'Arto Hellas',
+    phone: '040-123543',
+    street: 'Tapiolankatu 5 A',
+    city: 'Espoo',
+    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
   },
   {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
+    name: 'Matti Luukkainen',
+    phone: '040-432342',
+    street: 'Malminkaari 10 A',
+    city: 'Helsinki',
+    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
   },
   {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
+    name: 'Venla Ruuska',
+    street: 'Nallemäentie 22 C',
+    city: 'Helsinki',
+    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
   },
 ]
 
@@ -118,25 +118,22 @@ const resolvers = {
   Query: {
     personCount: async () => Person.collection.countDocuments(),
     allPersons: async (root, args, context) => {
-console.log('persons', args.name)
-    //   if (!args.phone) {
-    //     return persons
-    //   }
-    //   const byPhone = (person) =>
-    //     args.phone === 'YES' ? person.phone : !person.phone
-    //   return persons.filter(byPhone)
-    // },
+      //   if (!args.phone) {
+      //     return persons
+      //   }
+      //   const byPhone = (person) =>
+      //     args.phone === 'YES' ? person.phone : !person.phone
+      //   return persons.filter(byPhone)
+      // },
       if (!args.phone) {
-  
         return Person.find({})
       }
-
-      return Person.find({ phone: { $exists: args.phone === 'YES'  }})
+      return Person.find({ phone: { $exists: args.phone === 'YES' } })
     },
     findPerson: async (root, args) => Person.findOne({ name: args.name }),
     me: (root, args, context) => {
       return context.currentUser
-    }
+    },
   },
   Person: {
     address: ({ street, city }) => {
@@ -150,12 +147,12 @@ console.log('persons', args.name)
     addPerson: async (root, args, context) => {
       const person = new Person({ ...args })
       const currentUser = context.currentUser
-
+      console.log('person', person, 'currentUser', currentUser)
       if (!currentUser) {
         throw new GraphQLError('not authenticated', {
           extensions: {
             code: 'BAD_USER_INPUT',
-          }
+          },
         })
       }
 
@@ -168,11 +165,11 @@ console.log('persons', args.name)
           extensions: {
             code: 'BAD_USER_INPUT',
             invalidArgs: args.name,
-            error
-          }
+            error,
+          },
         })
       }
-  
+
       return person
     },
     editNumber: async (root, args) => {
@@ -185,62 +182,73 @@ console.log('persons', args.name)
           extensions: {
             code: 'BAD_USER_INPUT',
             invalidArgs: args.name,
-            error
-          }
+            error,
+          },
         })
       }
       return person
     },
     createUser: async (root, args) => {
       const user = new User({ username: args.username })
-  
-      return user.save()
-        .catch(error => {
-          throw new GraphQLError('Creating the user failed', {
-            extensions: {
-              code: 'BAD_USER_INPUT',
-              invalidArgs: args.name,
-              error
-            }
-          })
+
+      return user.save().catch((error) => {
+        throw new GraphQLError('Creating the user failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error,
+          },
         })
+      })
     },
     login: async (root, args) => {
       const user = await User.findOne({ username: args.username })
-  
-      if ( !user || args.password !== 'secret' ) {
+
+      if (!user || args.password !== process.env.PASSWORD) {
+        console.log(
+          'user',
+          user,
+          'args.username',
+          args.username,
+          'password',
+          args.password
+        )
+        console.log('Im here 1')
         throw new GraphQLError('wrong credentials', {
-          extensions: { code: 'BAD_USER_INPUT' }
-        })        
+          extensions: { code: 'BAD_USER_INPUT' },
+        })
       }
-  
+
       const userForToken = {
         username: user.username,
         id: user._id,
       }
-  
+
       return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
     },
     addAsFriend: async (root, args, { currentUser }) => {
-      const nonFriendAlready = (person) => 
-        !currentUser.friends.map(f => f._id.toString()).includes(person._id.toString())
-  
+      const nonFriendAlready = (person) =>
+        !currentUser.friends
+          .map((f) => f._id.toString())
+          .includes(person._id.toString())
+
       if (!currentUser) {
+        console.log('Im here 2')
         throw new GraphQLError('wrong credentials', {
-          extensions: { code: 'BAD_USER_INPUT' }
-        }) 
+          extensions: { code: 'BAD_USER_INPUT' },
+        })
       }
-  
+
       const person = await Person.findOne({ name: args.name })
-      if ( nonFriendAlready(person) ) {
+      if (nonFriendAlready(person)) {
         currentUser.friends = currentUser.friends.concat(person)
       }
-  
+
       await currentUser.save()
-  
+
       return currentUser
     },
-  }
+  },
 }
 
 const server = new ApolloServer({
@@ -252,12 +260,11 @@ startStandaloneServer(server, {
   listen: { port: 4000 },
   context: async ({ req, res }) => {
     const auth = req ? req.headers.authorization : null
-    if (auth && auth.startsWith('Bearer ')) {
-      const decodedToken = jwt.verify(
-        auth.substring(7), process.env.JWT_SECRET
+    if (auth && auth.startsWith('Bearer')) {
+      const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+      const currentUser = await User.findById(decodedToken.id).populate(
+        'friends'
       )
-      const currentUser = await User
-        .findById(decodedToken.id).populate('friends')
       return { currentUser }
     }
   },
--- graphql-mongoDb-phonebook-backend/models/person.js
+++ graphql-mongoDb-phonebook-backend/models/person.js
@@ -1,40 +1,68 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI
-
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
+// const schema = new
+// mongoose.Schema({
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 3,
     required: true,
-    unique: true,
+    minlength: 5,
   },
-  number: {
+  phone: {
     type: String,
-    minlength: 8,
-    required: true,
+    minlength: 5,
   },
-  date: Date,
-})
-
-personSchema.plugin(uniqueValidator)
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+  street: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  city: {
+    type: String,
+    required: true,
+    minlength: 3,
   },
 })
 
 module.exports = mongoose.model('Person', personSchema)
+
+// const mongoose = require('mongoose')
+// const uniqueValidator = require('mongoose-unique-validator')
+
+// const url = process.env.MONGODB_URI
+
+// mongoose
+//   .connect(url)
+//   .then(() => {
+//     console.log('connected to MongoDB')
+//   })
+//   .catch((error) => {
+//     console.log('error connecting to MongoDB:', error.message)
+//   })
+
+// const personSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     minlength: 3,
+//     required: true,
+//     unique: true,
+//   },
+//   number: {
+//     type: String,
+//     minlength: 8,
+//     required: true,
+//   },
+//   date: Date,
+// })
+
+// personSchema.plugin(uniqueValidator)
+
+// personSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   },
+// })
+
+// module.exports = mongoose.model('Person', personSchema)
